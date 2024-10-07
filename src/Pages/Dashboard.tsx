ce4cb8e7ada401583539ddb632e4fa66812daad6
Pascal <pbienenstein@posteo.de>
2024-10-07 16:37:52 +0200

    wip

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -5,8 +5,10 @@ import "../App.css";  // Füge eine CSS-Datei hinzu
 
 const Dashboard: FC = () => {
     return (
-        <Container className="container-sm d-flex flex-column justify-content-center align-items-center text-center p-3">
-            <Image src={startbild} rounded className="responsive-image" />
+        <Container
+            className="container-sm d-flex flex-column justify-content-center align-items-center text-center p-3">
+            <h1 className={"text-dark-orange"}>Tennisclub Mundelsheim</h1>
+            <Image src={startbild} rounded className="responsive-image"/>
         </Container>
     );
 };

261ec59693fb7dbe3a1afcfb7a50d17aad1b4b05
Pascal <pbienenstein@posteo.de>
2024-10-07 18:31:57 +0200

    navbar color orange

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, Nav, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle, NavDropdown } from "react-bootstrap";
-import { Link } from "react-router-dom"; // Import Link instead of NavLink
+import { Link } from "react-router-dom";
+import '../App.css'; // Ensure you import your CSS file
 
 const Navigation = () => {
     return (
-        // <Navbar expand={"lg"} className={"bg-body-tertiary"} bg={"light"} data-bs-theme={"light"}>
-        <Navbar expand={"lg"} style={{ backgroundColor: "#EC7600" }} data-bs-theme={"light"}>
+        <Navbar expand={"lg"} style={{ backgroundColor: "#EC7600" }}>
             <Container>
                 <NavbarBrand as={Link} to="/tc-mundelsheim">TC-Mundelsheim</NavbarBrand>
                 <NavbarToggle aria-controls={"basic-navbar-nav"} />
@@ -13,31 +13,31 @@ const Navigation = () => {
                     <Nav>
                         {/* Über uns */}
                         <NavDropdown title="Über uns" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/verein">Der Verein</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/mitgliedschaft">Mitgliedschaft</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/jugend">Jugend</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/verein">Der Verein</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/mitgliedschaft">Mitgliedschaft</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/jugend">Jugend</NavDropdown.Item>
                         </NavDropdown>
 
                         {/* Sport und Training */}
                         <NavDropdown title="Sport und Training" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/mannschaften">Mannschaften</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/training">Training</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/ergebnisse">Ergebnisse</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/tennisschule">Tennisschule</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/mannschaften">Mannschaften</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/training">Training</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/ergebnisse">Ergebnisse</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/tennisschule">Tennisschule</NavDropdown.Item>
                         </NavDropdown>
 
                         {/* Aktuelles */}
                         <NavDropdown title="Aktuelles" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/termine">Termine</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/aktuelles">Aktuelles</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/berichte">Berichte</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/termine">Termine</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/aktuelles">Aktuelles</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/berichte">Berichte</NavDropdown.Item>
                         </NavDropdown>
 
                         {/* Services */}
                         <NavDropdown title="Service" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/putzdienst">Putzdienst</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/anfahrt">Anfahrt</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/service">Service und Links</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/putzdienst">Putzdienst</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/anfahrt">Anfahrt</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item-orange" as={Link} to="/service">Service und Links</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </NavbarCollapse>
